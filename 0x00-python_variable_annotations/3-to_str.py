f2c311b6f2b285c7d5479b6bac51178bb20e0aa7
masilelazile <masilelazile@gmail.com>
2024-03-15 05:05:44 -0700

    God

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string  """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the float. """
+    return str(n)#!/usr/bin/env python3
+""" Basic annotations - to string  """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the float. """
+    return str(n)
